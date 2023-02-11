ef97128dcf0b1887fb26ef318d519dcceeb7a7b6
ndallich1 <ndallich@gmail.com>
2023-02-11 12:53:09 -0500

    updated server.js

---
+++ helpers/uuid.js
@@ -0,0 +1,5 @@
+// a function that generates a string of random numbers and letters
+const id = () =>
+  Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const { readFromFile, readAndAppend } = require("./helpers/fsUtils");
+const dbJSON = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,6 +24,41 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+app.post("api/notes", (req, res) =>
+  res.json(`${req.method} request received to add note`)
+);
+
+app.get("/api/notes", (req, res) => res.json(dbJSON));
+
+app.post("/api/notes", (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  // Destructuring assignment for the items in req.body
+  const { title, text, id } = req.body;
+
+  // If all the required properties are present
+  if (req.body) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    dbJSON.push(newNote);
+
+    readAndAppend(newNote, "./db/db.json");
+
+    res.json("Note added successfully");
+  } else {
+    res.json("Error in posting note");
+  }
+});
+
+// app.delete("/api/notes/:id", (req, res) => res.send(`DELETE route`));
+app.delete("/api/notes/:id", (req, res) => {
+  res.send("DELETE Request Called");
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
